5e3cbd6a8426a62aea57107d83ce711d27e5991f
Mobashir Farhan <farhanmobashir007@gmail.com>
2021-04-29 23:45:01 +0530

    adds context and theme

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,24 @@
+import { addParameters, addDecorators, addDecorator } from "@storybook/react";
+import { withContexts } from "@storybook/addon-contexts";
+import { contexts } from "./contexts";
 
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-}
+// post-written
+addParameters({
+  backgrounds: [
+    { name: "Dafault Theme", value: "#ffffff", default: true },
+    { name: "Dark Theme", value: "#7F2EAB" },
+  ],
+});
+
+addDecorator(withContexts(contexts));
+
+// Pre-written
+// export const parameters = {
+//   actions: { argTypesRegex: "^on[A-Z].*" },
+//   controls: {
+//     matchers: {
+//       color: /(background|color)$/i,
+//       date: /Date$/,
+//     },
+//   },
+// };
